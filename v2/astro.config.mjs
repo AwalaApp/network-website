f9ba12b7be342a688f1460517cce47ecc4d3cd29
Gus Narea <gnarea@users.noreply.github.com>
2024-09-04 13:30:53 +0100

    Integrate ESLint and Prettier

--- v2/astro.config.mjs
+++ v2/astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    i18n: {
-        defaultLocale: "en",
-        locales: ["en", "zh-cn"],
-        routing: {
-            prefixDefaultLocale: true,
-            redirectToDefaultLocale: false,
-        },
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "zh-cn"],
+    routing: {
+      prefixDefaultLocale: true,
+      redirectToDefaultLocale: false,
     },
+  },
 });
